c586c04021349c01235a18f1a212a03e5539d844
Iisakki Säkkinen <esakkinen@topcon.com>
2024-01-16 20:28:24 +0200

    Autentikointi poistolle

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -33,6 +33,12 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
     const id = request.params.id
 
     await Blog.findByIdAndRemove(id)
