60127b39265129a9e67d52c3dccc7e83d595c8ff
Iisakki Säkkinen <esakkinen@topcon.com>
2024-01-16 11:31:32 +0200

    apufunktioita ja yksikkötestejä step4 valmis

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,11 +2,24 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blogs) => sum + blogs.likes, 0)
-}
+const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0)
+
+const favoriteBlog = (blogs) => blogs.reduce((favorite, blog) => favorite === null || favorite.likes < blog.likes ? blog : favorite, null)
+
+const mostBlogs = (blogs) => blogs.reduce((bloggers, blog) => {
+    bloggers[blog.author] = {
+        author: blog.author,
+        blogs: bloggers[blog.author] === undefined ? 1 : bloggers[blog.author].blogs + 1
+    }
+    bloggers.mostBlogs = bloggers.mostBlogs === undefined || bloggers[blog.author].blogs > bloggers.mostBlogs.blogs
+        ? bloggers[blog.author]
+        : bloggers.mostBlogs
+    return bloggers
+}, {}).mostBlogs || null
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
